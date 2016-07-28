9a3eeef77fa0fef081e93bceb0b01110f2ded479
#SundzMurim <#SundzMurim@Sharingan>
2016-07-28 11:38:33 +0200

    Milestone 02

--- TodoListManager/src/com/example/todolistmanager/RootServlet.java
+++ TodoListManager/src/com/example/todolistmanager/RootServlet.java
@@ -31,7 +31,41 @@ public class RootServlet extends HttpServlet {
 		// set the response type to be html text
 		resp.setContentType("text/html");
 		
-	
+		// get access to the google user service
+		UserService us = UserServiceFactory.getUserService();
+		User user = us.getCurrentUser();
+		String login_url = us.createLoginURL("/");
+		String logout_url = us.createLogoutURL("/");
+		
+		// attach a few things to the request such that we can access them in the jsp
+		req.setAttribute("user", user);
+		req.setAttribute("login_url", login_url);
+		req.setAttribute("logout_url", logout_url);
+				
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		//MemcacheService ms = MemcacheServiceFactory.getMemcacheService();
+			
+		// get access to the user. if they do not exist in the datastore then
+		// store a default version of them. of course we have to check that a user has logged in first
+		UserT existingUser=null;
+		if(user != null) {
+			Key userKey = KeyFactory.createKey(UserT.class.getSimpleName(), user.getUserId());
+			try {
+	            existingUser = pm.getObjectById(UserT.class, userKey);
+	        } catch (JDOObjectNotFoundException e) {
+	        	existingUser = new UserT();
+	        	existingUser.setID(userKey);
+	            existingUser.setEmail(user.getEmail());
+	            pm.makePersistent(existingUser);
+	        }	
+			
+			req.setAttribute("currentUser",existingUser);
+			//List<Todo> todoList = existingUser.getTodos();
+			
+			pm.close();
+
+			System.out.println("user is: " + existingUser.getId());
+		}
 
 		// get access to a request dispatcher and forward onto the root.jsp file
 		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/root.jsp");
@@ -42,5 +76,31 @@ public class RootServlet extends HttpServlet {
 	// simple post method that updates a users preferences with new email information
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		
+		UserService us = UserServiceFactory.getUserService();
+		User user = us.getCurrentUser();
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		
+		// get access to the user. if they do not exist in the datastore then
+		// store a default version of them. of course we have to check that a user has
+		// logged in first
+		UserT existingUser=null;
+		if(user != null) {
+			
+			Key userKey = KeyFactory.createKey(UserT.class.getSimpleName(), user.getUserId());
+			try {
+	            existingUser = pm.getObjectById(UserT.class, userKey);
+	        } catch (JDOObjectNotFoundException e) {
+	            //existingUser = new UserT(user.getEmail());
+	            existingUser = new UserT();
+	        	existingUser.setID(userKey);
+	        	existingUser.setEmail(user.getEmail());
+	            pm.makePersistent(existingUser);
+	        }
+			
+			pm.close();
+		}
+				
+		resp.sendRedirect("/");
+
 	}	
 }
--- TodoListManager/war/WEB-INF/root.jsp
+++ TodoListManager/war/WEB-INF/root.jsp
@@ -1,27 +1,57 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="com.google.appengine.api.users.User" %>
+<%@ page import="com.google.appengine.api.users.UserService" %>
+<%@ page import="com.google.appengine.api.users.UserServiceFactory" %>
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="com.google.appengine.api.datastore.DatastoreService" %>
+<%@ page import="com.google.appengine.api.datastore.DatastoreServiceFactory" %>
+<%@ page import="com.google.appengine.api.datastore.Entity" %>
+<%@ page import="com.google.appengine.api.datastore.FetchOptions" %>
+<%@ page import="com.google.appengine.api.datastore.Key" %>
+<%@ page import="com.google.appengine.api.datastore.KeyFactory" %>
+<%@ page import="com.google.appengine.api.datastore.Query" %>
+<%@ page import="com.example.todolistmanager.UserT" %>
+
 
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
 		<title>TODO LIST MANAGER</title>
+		<base href="${pageContext.request.contextPath}">
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
 	</head>
 	<body>
-	
-	<c:choose>
-		<c:when test = " ${user != null } ">
-			<p>
-				Welcome ${user.email} <br/>
-				You can signout <a href="${logout_url}">here</a><br/>
-			</p>
-		</c:when>
-		<c:otherwise>
-			<p>
-				Welcome!
-				<a href="${login_url}">Sign in or register</a>
-			</p>
-		</c:otherwise>
-	</c:choose>
-	
+		<!-- if the user is logged in then we need to render one version of the page
+		consequently if the user is logged out we need to render a different version of the page -->
+		
+		<%
+		    String userName = request.getParameter("user");
+			UserT currentUser = (UserT)request.getAttribute("currentUser");
+		    
+			if (userName == null) {
+		        userName = "default";
+		    }
+		    
+		    pageContext.setAttribute("user", userName);
+		    UserService userService = UserServiceFactory.getUserService();
+		    User user = userService.getCurrentUser();
+		
+			if (user != null) {
+				pageContext.setAttribute("user", user);
+		%>
+				<br/>Welcome ${user.nickname} ! 
+				<a href="${logout_url}">Sign out</a><br/><br/>
+				
+				
+		<%
+			} else {
+		%>
+				<p>Welcome!<a href="${login_url}">Sign in or register</a></p>	
+		<%
+			}
+		%>
+		
 	</body>
 </html>
